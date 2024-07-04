46e94e03222d662ecdc6a51f46586e49e50e5344
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-07-04 01:16:20 +0100

    almost done

--- js/index.js
+++ js/index.js
@@ -2,8 +2,20 @@ const menu = document.querySelector('.menu');
 const menumobile = document.querySelector('.menu-mobile');
 menu.addEventListener('click', () => {
     menumobile.style.display = "block";
+    menumobile.style.display = "flex";
+    menumobile.style.animation="move 2s ease 1";
 })
 const close = document.querySelector('.close');
 close.addEventListener('click', () => {
-    menumobile.style.display = "none";
+    menumobile.style.animation = "moveout 1s ease 1";
+    setTimeout(() => {
+        menumobile.style.display = "none";
+    }, 900);
+})
+const a = document.querySelector('.menu-mobile a');
+a.addEventListener('click', () => {
+    menumobile.style.animation = "moveout 6.1s ease 1";
+    setTimeout(() => {
+        menumobile.style.display = "none";
+    }, 6000);
 })
