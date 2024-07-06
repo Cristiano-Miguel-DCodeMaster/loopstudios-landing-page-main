d3b4f8c2f8f71ff9c7d865797275ac8cd0125532
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-07-06 18:31:29 +0100

    I added some scroll animations

--- js/index.js
+++ js/index.js
@@ -21,3 +21,17 @@ a.forEach(a => {
         }, 6000);
     })
 });
+const section = document.querySelectorAll('section');
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        
+        if (top >= offset && top < offset + height ) {
+            sec.classList.add('show');
+        } else {
+            sec.classList.remove('show');
+        }
+    })
+}
